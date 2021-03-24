1635e49c869b1f356b4ade580e43a1bc81adee71
czzbandicoot <victorsuleiman.1994@gmail.com>
2021-03-24 14:52:57 -0700

    added a hardcoded json from backend for testing, some comments

--- app/src/main/java/com/example/pickrecipe/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/pickrecipe/ui/home/HomeFragment.kt
@@ -12,6 +12,38 @@ import com.example.pickrecipe.R
 
 class HomeFragment : Fragment() {
 
+    /*
+    Classes Used in test:
+
+        Raw data file: recipes.json. I will need to replace this with data coming from the backend
+
+        Entities:
+            Moshi: RecipeJson
+            Room and Frontend: RecipeEntity (inside model folder inside db folder lol)
+
+        JsonReader and Entity Converter: RecipeJsonReaderTester inside json folder
+        Adapter: RecipeAdapter inside adapters folder
+        Fragment: ListFragmentRecipe inside list folder inside fragment folder
+        ViewModel: RecipeViewModel inside viewmodel folder
+        Repository: RecipeRepository inside repository folder
+        Dao: RecipeDao inside data folder inside db folder
+        DB: RecipeIngredientDatabase inside those same folders
+
+
+        Functionality:
+            viewModel will access the database with the dao and insert all data using repository if not inserted
+            them it will read all data that will come into a list of recipes, set that list to the adapter
+
+        TODO:
+            create the recipe database inside mongoDB and properly access it using socket, probably in the RecipeJsonReaderTester.
+            modify both Moshi and room entities and uninstall the app to reload new DB schema
+            see if I need to modify any data on the top-level view with the new attributes, see the adapter
+            copy codes from viewModels and fragment. Good luck to me lol
+            qd eu for converter o json eu dou parse nas strings com delimitador
+
+
+    */
+
     private lateinit var homeViewModel: HomeViewModel
 
     override fun onCreateView(
